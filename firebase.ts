8f6cde99e2bb23a4b2953c29392f44ee2cd68712
ahmed.atya.daif <ahmed.atya.daif@gmail.com>
2025-10-13 16:54:13 +0000

    fix(app)!: Reconnect app to correct Firebase project

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
+// Your web app's CORRECT Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeI2YL1ChK_93eL6nfcA3RvbCg0xP9rZU",
-  authDomain: "cleanmaster-app-65622056-6e2da.firebaseapp.com",
-  projectId: "cleanmaster-app-65622056-6e2da",
-  storageBucket: "cleanmaster-app-65622056-6e2da.firebasestorage.app",
-  messagingSenderId: "117385968185",
-  appId: "1:117385968185:web:2040b754613678c4532be8"
+  apiKey: "AIzaSyDZJ_uzBhwPOcvmsWXFrg-1ovDc7YzKMmE",
+  authDomain: "clean-master-16883.firebaseapp.com",
+  projectId: "clean-master-16883",
+  storageBucket: "clean-master-16883.firebasestorage.app",
+  messagingSenderId: "22734760869",
+  appId: "1:22734760869:web:3a103ec69554b7bc48d64c",
+  measurementId: "G-XM7Q2PX8VR"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
